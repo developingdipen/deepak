73e2b0f0c5a7d9584b0d1a561280a3e9a7e4fcf5
Dipen Patel <60754812+developingdipen@users.noreply.github.com>
2020-04-25 22:13:15 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+		$('.heroimage').ripples({
+			resolution: 256,
+			perturbance: 0.03
+		});
+    })
+
+
+    function myFunction() {
+        document.getElementById("nav").scrollIntoView()
+    }
