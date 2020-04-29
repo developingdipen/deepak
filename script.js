640ddf250b471019412197a6014e3ad271ca7a6f
Dipen Patel <60754812+developingdipen@users.noreply.github.com>
2020-04-29 17:25:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-		$('.heroimage').ripples({
-			resolution: 256,
-			perturbance: 0.03
-		});
-    })
-
-
     function myScroll() {
         document.getElementById("nav").scrollIntoView()
     }
